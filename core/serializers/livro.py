93720c974aaf42ad46f3488af2b1c00b1fb745ee
juliacdss <juliacostaifc@gmail.com>
2026-04-10 10:57:59 -0300

    feat: criação de múltiplos serializadores para Livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -10,7 +10,13 @@ class LivroSerializer(ModelSerializer):
         depth = 1
 
 
-class LivroListRetrieveSerializer(ModelSerializer):
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ('id', 'titulo', 'preco')
+
+
+class LivroRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
