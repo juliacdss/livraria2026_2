3401bd860e0d9f5166acedeb17f2ad9d26916d7d
juliacdss <juliacostaifc@gmail.com>
2026-04-17 11:07:11 -0300

    feat: inclusão da app de upload e associação de imagens

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,9 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from core.models import Livro
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 
 class LivroSerializer(ModelSerializer):
+    apa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Livro
         fields = '__all__'
@@ -17,6 +28,8 @@ class LivroListSerializer(ModelSerializer):
 
 
 class LivroRetrieveSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
     class Meta:
         model = Livro
         fields = '__all__'
