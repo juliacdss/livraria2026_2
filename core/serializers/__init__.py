e556cdd50462b06a7c888db793aa1bd53a139721
juliacdss <juliacostaifc@gmail.com>
2026-08-21 10:29:49 -0300

    feat: criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,9 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
-from .compra import CompraSerializer, ItensCompraSerializer
+from .compra import (
+    CompraSerializer, 
+    ItensCompraSerializer,
+    ItensCompraCreateUpdateSerializer,
+    CompaCreateUpdateSerializer
+)
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,6 +1,14 @@
+from django.db import transaction
 from rest_framework.serializers import CharField, DecimalField, ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
+
+
+#  Itens Compra
+class ItensCompraCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ('livro', 'quantidade')
 
 
 class ItensCompraSerializer(ModelSerializer):
@@ -24,6 +32,8 @@ class ItensCompraSerializer(ModelSerializer):
         fields = ('livro', 'quantidade', 'total')
         depth = 1
 
+# Compra
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
@@ -33,3 +43,20 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ('id', 'usuario', 'status', 'total', 'itens')
+
+
+class CompraCreateUpdateSerializer(ModelSerializer):
+    itens = ItensCompraCreateUpdateSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ('id', 'usuario', 'itens')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        itens_data = validated_data.pop('itens')
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
